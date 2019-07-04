1a1d5aff85c92fe99ea0a7a51e165ffc3d915503
Allan <ferrerallan@gmail.com>
2019-07-04 13:58:05 -0300

    teste

--- TesteRecomenda.py
+++ TesteRecomenda.py
@@ -2,21 +2,21 @@ from base import avaliacoes
 
 from math import sqrt
 
-def euclidiana(usuario1,usuario2):
+def euclidiana(procedimento1,procedimento2):
     si = {}
-    for item in avaliacoes[usuario1]:
-        if item in avaliacoes[usuario2]:si[item]=1
+    for item in avaliacoes[procedimento1]:
+        if item in avaliacoes[procedimento2]:si[item]=1
 
     if len(si)==0: return 0
 
-    soma = sum([pow(avaliacoes[usuario1][item] - avaliacoes[usuario2][item],2)
-                for item in avaliacoes[usuario1] if item in avaliacoes[usuario2]])
+    soma = sum([pow(avaliacoes[procedimento1][item] - avaliacoes[procedimento2][item],2)
+                for item in avaliacoes[procedimento1] if item in avaliacoes[procedimento2]])
 
     return 1/(1+sqrt(soma))
 
-def getSimilares(usuario):
-    similaridade=[(euclidiana(usuario,outro), outro)
-                    for outro in avaliacoes if outro != usuario]
+def getSimilares(procedimento):
+    similaridade=[(euclidiana(procedimento,outro), outro)
+                    for outro in avaliacoes if outro != procedimento]
     similaridade.sort()
     similaridade.reverse()
     return similaridade
